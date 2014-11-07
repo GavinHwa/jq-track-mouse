a86047cd043d763fec4bd5f117bc03d1a61ec88d
Kamran Ahmed <kamranahmed.se@gmail.com>
2014-11-07 08:04:24 +0500

    Complete the basic plugin functionality

--- jq-track-mouse.js
+++ jq-track-mouse.js
@@ -27,14 +27,71 @@ if ( typeof Object.create !== 'function' ) {
 
 			// Start the processing, iff text provided
 			if ( this.options.text ) {
-
+				this.generateTracker();
 				this.bindUI();
-			};
+				this.showTracker();
+			}
 		},
 		
 		bindUI: function () {
 
+			var self = this;
 
+			$(document).on('mousemove', function( e ){
+				$(document).trigger('trackmouse', e);
+			});
+
+			$(document).on('trackmouse', function( e, mouseObj ){
+				self.moveTracker( mouseObj.pageX, mouseObj.pageY );
+			});
+		},
+
+		generateTracker: function () {
+
+			if ( $('.jq-mouse-tracker').length != 0 ) {
+				$('.jq-mouse-tracker').remove();
+			}
+				
+			this.el = $('<div class="jq-mouse-tracker"></div>');
+			this.el.css({
+				'position' : 'fixed',
+				'display' : 'none',
+				'left' : 0,
+				'top' : 0,
+				'z-index' : 9999999999,
+				'font-family' : 'tahoma, arial, sans-serif',
+				'font-size' : '14px',
+				'background-color' : '#444',
+				'color' : '#eee',
+				'padding' : 10
+			});
+
+			$('body').append( this.el );
+
+			if ( ( typeof this.options.text == 'string' ) || ( typeof this.options.text == 'number' ) ) {
+				this.el.html( this.options.text );
+			} else if ( this.options.text instanceof Array ) {
+				var text = "<ul><li>" + this.options.text.join('</li><li>') + "</li></ul>";
+				this.el.html( text );
+			} else {
+				this.options.text = 'Invalid text format, you can only provide string or array.';
+			}
+		},
+
+		moveTracker: function ( x, y ) {
+
+			this.el.css({
+				'left' : x + this.options.offset.x,
+				'top' : y + this.options.offset.y
+			});
+		},
+
+		showTracker: function () {
+			this.el.show();
+		},
+
+		reset: function () {
+			this.el.remove();
 		}
 	};
 	
@@ -44,13 +101,17 @@ if ( typeof Object.create !== 'function' ) {
 
 		return {
 			reset : function () {
-				
+				trackMouse.reset();
 			}
 		}
 	};
 
 	$.trackMouse.options = {
-		text : ''
+		text : 'Provide some text',
+		offset : {
+			x : 20,
+			y : 20
+		}
 	};
 
 })( jQuery, window, document );
